e266cae10545d3a6cac6d5301b3d4d2fd6c1345c
MrEmin <emuhammet.ozdemir@gmail.com>
2023-07-31 08:43:51 +0300

    About, Checkout Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='About' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus officiis, fuga ad recusandae reprehenderit ratione
+            rem magni quae commodi ab tempora, ipsa laudantium saepe itaque
+            vitae architecto voluptatem neque aut.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
