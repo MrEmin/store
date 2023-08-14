ad06df075cc486c93080f1cfc980a31c5e9da9a2
MrEmin <emuhammet.ozdemir@gmail.com>
2023-08-14 06:53:23 +0300

    Cart Page Setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -25,8 +25,19 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
 
+  // remove item
+  const removeItem = (id) => {}
+
+  // toggle amount
+  const toggleAmount = (id, value) => {}
+
+  // clear item
+  const clearItem = () => {}
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearItem }}
+    >
       {children}
     </CartContext.Provider>
   )
