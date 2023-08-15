f9470738890aa7eef9b5a925ca163e38f48a561e
MrEmin <emuhammet.ozdemir@gmail.com>
2023-08-15 14:49:14 +0300

    Stripe Checkout - Setup

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,15 @@
 // domain/.netlify/functions/hello
-
+// bu api'nin totalde 125k istek hakkı var
+/* const items = [
+  { id: 1, name: 'Emin' },
+  { id: 2, name: 'Suzan' },
+  { id: 3, name: 'Ali' },
+]
+ */
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: 'Hello World',
+    // body: JSON.stringify(items),
+    body: 'hello world',
   }
 }
